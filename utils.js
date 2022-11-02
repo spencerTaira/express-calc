e7c7b1ea6a9aca99e8bf7d488491cda5e88cf8ec
Spencer Taira <spencermtaira@gmail.com>
2022-11-02 12:10:25 -0700

    finished functionality for convertStrNums

--- utils.js
+++ utils.js
@@ -1,11 +1,22 @@
+"use strict";
 const { BadRequestError } = require("./expressError");
 
 
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
-  // if the conversion isn't successful, throw a BadRequestError and will
-  // be handled in your route
+  console.log('convertStrNums');
+  console.log('input', strNums);
+  const nums = strNums.map(el => {
+    console.log('el', Number(el));
+    if (isNaN(Number(el))) {
+      console.log('NaN');
+      throw new BadRequestError(`${el} is not a number!`);
+    }
+    return Number(el);
+  });
+
+  return nums;
 }
 
 
